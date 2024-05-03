92d15e88868c0ce0bf3317a61ed1ab42aa00a2bc
Elif Sıla Taş <122424595+elifsilatas@users.noreply.github.com>
2024-05-04 01:05:51 +0300

    filmSecim

---
+++ Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MenuStrip_Kullanimi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void maviToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.CadetBlue;
+        }
+
+        private void griToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.Gray;
+        }
+
+        private void turuncuToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.Orange;
+        }
+
+        private void hakkımızdaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Bu proje Elif Sıla tarafından yapıldı", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void çıkışToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void madMaxToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("https://youtu.be/hEJnMQG9ev8?si=GA6OwEUBuCAH3nqs");
+            label1.Text = "Mad Max";
+        }
+
+        private void psILoveYouToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("https://youtu.be/CZzW6_hR068?si=QgrQLcY7qQftyLlu");
+            label1.Text = "Ps: I Love You";
+        }
+
+        private void aynıYıldızınAtındaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("https://youtu.be/aQnlngjZG48?si=DZyIpQCJvV_xbr0N");
+            label1.Text = "Aynı Yıldızın Altında";
+        }
+
+        private void korkuSeansıToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("https://youtu.be/5u5A471adxg?si=un1p9RE0bft-0_7M");
+            label1.Text = "Korku Seansı";
+        }
+
+        private void uzayYolcularıToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("https://www.youtube.com/watch?v=7BWWWQzTpNU&ab_channel=SonyPicturesEntertainment");
+            label1.Text = "Uzay Yolcuları";
+        }
+    }
+}
